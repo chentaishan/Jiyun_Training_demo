6739dcae6be0046fc796250505c24c1ac9d6d980
chents <chentaishan-19@163.com>
2020-03-04 17:27:14 +0800

    使用弱引用加载view,避免内存泄漏

--- app/src/main/java/com/example/jiyun_training_demo/base/BasePresenter.java
+++ app/src/main/java/com/example/jiyun_training_demo/base/BasePresenter.java
@@ -1,13 +1,22 @@
 package com.example.jiyun_training_demo.base;
 
+import java.lang.ref.WeakReference;
+
 public abstract class BasePresenter<V extends IBaseView> implements IBasePresenter<V> {
 
     protected V view;
+    private WeakReference<V> vWeakReference;
 
 
+    /**
+     * 使用弱引用加载view,避免内存泄漏
+     * @param baseView
+     */
     @Override
     public void attachView(V baseView) {
-        view = baseView;
+
+        vWeakReference = new WeakReference<>(baseView);
+        view= vWeakReference.get();
     }
 
     @Override
