ce423393c149eebb816064957ba95bd3deb7c618
chents <chentaishan-19@163.com>
2020-03-05 20:08:38 +0800

    添加日志 网络拦截器

--- app/src/main/java/com/example/jiyun_training_demo/service/HttpManager.java
+++ app/src/main/java/com/example/jiyun_training_demo/service/HttpManager.java
@@ -1,9 +1,18 @@
 package com.example.jiyun_training_demo.service;
 
+import android.util.Log;
+
 import com.example.jiyun_training_demo.base.MyApp;
+import com.example.jiyun_training_demo.utils.SystemUtils;
+
+import java.io.IOException;
 
 import okhttp3.Cache;
+import okhttp3.CacheControl;
+import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -42,8 +51,63 @@ public class HttpManager {
 
         final OkHttpClient build = new OkHttpClient.Builder()
                 .cache(new Cache(MyApp.myApp.getCacheDir(), 10 * 1024 * 1024))
+                .addInterceptor(new LoggingInterceptor())
+                .addNetworkInterceptor(new NetWorkInterceptor())
                 .build();
 
         return build;
+    }
+
+    class NetWorkInterceptor implements Interceptor{
+        Request request;
+        @Override
+        public Response intercept(Chain chain) throws IOException {
+
+
+            if (!SystemUtils.checkNetWork()){
+
+                  request = chain.request();
+
+                request.newBuilder().cacheControl(CacheControl.FORCE_CACHE).build();
+            }
+
+            Response response = chain.proceed(request);
+            if (!SystemUtils.checkNetWork()){
+
+                int maxAge=0;
+                return  response.newBuilder().removeHeader("Pragma").header("Cache-Control","public ,max-age="+maxAge).build();
+
+            }else{
+
+                int maxTime = 60*60 *24;
+
+              return  response.newBuilder().removeHeader("Pragma").header("Cache-Control","public ,max-age="+maxTime).build();
+
+            }
+
+
+        }
+    }
+
+    /**
+     * 日志拦截器
+     */
+    class LoggingInterceptor implements Interceptor{
+
+
+        @Override
+        public Response intercept(Chain chain) throws IOException {
+
+            long start = System.nanoTime();
+            Request request = chain.request();
+            Response proceed = chain.proceed(request);
+            long end = System.nanoTime();
+
+
+            Log.i("Request:",String.format("Sending request %s on %s%n%s",request.url(),chain.connection(),request.headers()));
+
+            Log.i("interceptor","time="+(end-start)+"ms");
+            return proceed;
+        }
     }
 }
